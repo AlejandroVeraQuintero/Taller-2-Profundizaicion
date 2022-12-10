9e30abf3cc26f8976c1dbc1b63c139ff33517c8b
Alejandro <veraa567@gmail.com>
2022-12-10 01:54:56 -0500

    first commit

---
+++ cinema/user-service/src/main/java/co/com/poli/userservice/persistence/entity/User.java
@@ -0,0 +1,41 @@
+package co.com.poli.userservice.persistence.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import java.util.Objects;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @NotEmpty(message = "El nombre no puede ser vacio")
+    @Column(name = "name")
+    private String name;
+
+    @NotEmpty(message = "El apellido no puede ser vacio")
+    @Column(name = "lastname")
+    private String lastname;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
